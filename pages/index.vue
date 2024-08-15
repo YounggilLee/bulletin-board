a28f268539af16d61b1cc6b78cd233ba305108b1
gil <iamleeyounggil@gmail.com>
2024-08-15 13:23:55 -0400

    install css framework, quasar

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,5 @@ const count = ref(444)
 </script>
 
 <template>
-  <div>
-    <a>Gil</a>
-    <strong>{{ count }}</strong>
-    <button @click="count++">Increase number + 1</button>
-  </div>
+  <q-page></q-page>
 </template>
