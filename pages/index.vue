0f4b6e190a22e47e6af0370fccfc9d4a6252511b
gil <iamleeyounggil@gmail.com>
2024-08-15 12:40:25 -0400

    set basic setting

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const count = ref(444)
+</script>
+
+<template>
+  <div>
+    <a>Gil</a>
+    <strong>{{ count }}</strong>
+    <button @click="count++">Increase number + 1</button>
+  </div>
+</template>
